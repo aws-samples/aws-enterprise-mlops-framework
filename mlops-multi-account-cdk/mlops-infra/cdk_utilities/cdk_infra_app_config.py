9fe4019609cd31a3ee0090be431f5c93df929474
Ravi Bhushan Ratnakar <ravibrat@amazon.de>
2023-10-10 08:59:24 +0200

    mlops_infra updated default value for configuration of create network

--- mlops-multi-account-cdk/mlops-infra/cdk_utilities/cdk_infra_app_config.py
+++ mlops-multi-account-cdk/mlops-infra/cdk_utilities/cdk_infra_app_config.py
@@ -14,6 +14,7 @@
 # HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+import os
 from dataclasses import dataclass, field
 from typing import List, Optional
 
@@ -25,6 +26,21 @@ DEFAULT_VALUE: str = 'default'
 
 @dataclass
 class CreateNetworkConfig(DataClassJsonMixin):
+    vpc_cidr: Optional[str] = field(default=None)
+    vpc_max_azs: Optional[int] = field(default=None)
+    vpc_private_subnet_cidr_mask: Optional[int] = field(default=None)
+    vpc_public_subnet_cidr_mask: Optional[int] = field(default=None)
+    vpc_enable_dns_hostnames: Optional[bool] = field(default=None)
+    vpc_enable_dns_support: Optional[bool] = field(default=None)
+    vpc_nat_gateways: Optional[int] = field(default=None)
+    vpc_add_s3_endpoint: Optional[bool] = field(default=None)
+    vpc_add_kms_endpoint: Optional[bool] = field(default=None)
+    vpc_add_ecr_endpoint: Optional[bool] = field(default=None)
+    vpc_add_ecr_docker_endpoint: Optional[bool] = field(default=None)
+
+
+@dataclass
+class DefaultCreateNetworkConfig(DataClassJsonMixin):
     vpc_cidr: str = field(default="10.0.0.0/16")
     vpc_max_azs: int = field(default=3)
     vpc_private_subnet_cidr_mask: int = field(default=24)
@@ -73,7 +89,7 @@ class NetworkConfig(DataClassJsonMixin):
             stages=[NetworkDeploymentStageConfig(stage_name=DEFAULT_VALUE)]
         )]
 
-    default_create_network: CreateNetworkConfig = field(default_factory=CreateNetworkConfig)
+    default_create_network: DefaultCreateNetworkConfig = field(default_factory=DefaultCreateNetworkConfig)
     deployments: List[NetworkDeploymentConfig] = field(default_factory=lambda: NetworkConfig.get_default_deployments())
 
     def get_network_stage_config_by(self, set_name: str, stage_name: str) -> Optional[NetworkDeploymentStageConfig]:
@@ -89,6 +105,44 @@ class NetworkConfig(DataClassJsonMixin):
             stage_name = DEFAULT_VALUE
 
         response = conf[0].get_network_stage_config_by(stage_name=stage_name)
+
+        # updating create_network value with updated default_create_network value in case where
+        # create_network doesn't have any or some of the attributes from config
+        if response:
+            cn: CreateNetworkConfig = response.create_network
+
+            if cn.vpc_cidr is None:
+                cn.vpc_cidr = self.default_create_network.vpc_cidr.strip()
+
+            if cn.vpc_max_azs is None:
+                cn.vpc_max_azs = self.default_create_network.vpc_max_azs
+
+            if cn.vpc_private_subnet_cidr_mask is None:
+                cn.vpc_private_subnet_cidr_mask = self.default_create_network.vpc_private_subnet_cidr_mask
+
+            if cn.vpc_public_subnet_cidr_mask is None:
+                cn.vpc_public_subnet_cidr_mask = self.default_create_network.vpc_public_subnet_cidr_mask
+
+            if cn.vpc_enable_dns_hostnames is None:
+                cn.vpc_enable_dns_hostnames = self.default_create_network.vpc_enable_dns_hostnames
+
+            if cn.vpc_enable_dns_support is None:
+                cn.vpc_enable_dns_support = self.default_create_network.vpc_enable_dns_support
+
+            if cn.vpc_nat_gateways is None:
+                cn.vpc_nat_gateways = self.default_create_network.vpc_nat_gateways
+
+            if cn.vpc_add_s3_endpoint is None:
+                cn.vpc_add_s3_endpoint = self.default_create_network.vpc_add_s3_endpoint
+
+            if cn.vpc_add_kms_endpoint is None:
+                cn.vpc_add_kms_endpoint = self.default_create_network.vpc_add_kms_endpoint
+
+            if cn.vpc_add_ecr_endpoint is None:
+                cn.vpc_add_ecr_endpoint = self.default_create_network.vpc_add_ecr_endpoint
+
+            if cn.vpc_add_ecr_docker_endpoint is None:
+                cn.vpc_add_ecr_docker_endpoint = self.default_create_network.vpc_add_ecr_docker_endpoint
 
         return response
 
@@ -128,3 +182,12 @@ class CdkInfraAppConfig(DataClassJsonMixin):
 @dataclass
 class InfraAppConfig(YamlDataClassConfig):
     cdk_infra_app_config: CdkInfraAppConfig = field(default_factory=CdkInfraAppConfig)
+
+
+base_dir: str = os.path.abspath(f'{os.path.dirname(__file__)}{os.path.sep}..')
+conf: InfraAppConfig = InfraAppConfig()
+
+config_filepath: str = os.path.join(base_dir, 'config', 'cdk-infra-app.yml')
+
+conf.load(config_filepath)
+print(f'conf : {str(conf.cdk_infra_app_config.network.get_network_stage_config_by("first-example", "dev"))}')
