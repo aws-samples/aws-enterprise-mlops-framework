6f61b66273045e6a6b5709d51a455fb84199e985
Ravi Bhushan Ratnakar <ravibrat@amazon.de>
2023-11-17 20:09:06 +0100

    cdk-app configuration creation using new aws accounts with isengard from cli

--- mlops-multi-account-cdk/mlops-commons/mlops_commons/utilities/config_helper.py
+++ mlops-multi-account-cdk/mlops-commons/mlops_commons/utilities/config_helper.py
@@ -19,9 +19,11 @@
 import os
 import logging
 import sys
+import yaml
+import argparse
 from logging import Logger
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Any
 
 from mlops_commons.utilities.cdk_app_config import (
     AppConfig,
@@ -34,6 +36,7 @@ class ConfigHelper(object):
     logging.basicConfig(level=logging.INFO)
 
     INSTANCE: 'ConfigHelper' = None
+    CONFIG_YAML_FILE_NAME: str = 'cdk-app.yml'
 
     def __init__(self):
 
@@ -50,7 +53,7 @@ class ConfigHelper(object):
         yaml_config_path: str = os.path.join(
             self.base_dir,
             'config',
-            'cdk-app.yml'
+            self.CONFIG_YAML_FILE_NAME
         )
 
         self.logger.info(f'Trying to loading cdk app configuration file : {yaml_config_path}')
@@ -72,8 +75,100 @@ class ConfigHelper(object):
     def get_config() -> CdkAppConfig:
         return ConfigHelper().app_config.cdk_app_config
 
+    @classmethod
+    def create_config_file(cls) -> None:
+
+        args = cls.parse_cli_args()
+        print(f'config args : {args}')
+
+        conf_dict = {
+            'cdk_app_config': {
+                'app_prefix': 'mlops',
+                'pipeline': {
+                    'account': args.gov_account,
+                    'region': args.gov_region,
+                    'bootstrap': {
+                        'aws_profile': args.gov_profile
+                    },
+                    'code_commit': {
+                        'infra': {
+                            'repo_name': 'mlops-infra',
+                            'branch_name': 'main'
+                        },
+                        'project_template': {
+                            'repo_name': 'mlops-sm-project-template',
+                            'branch_name': 'main'
+                        }
+                    }
+                },
+                'deployments': []
+
+            }
+        }
+        for bu, stages in [('first-example', [
+            ('dev', args.dev_account, args.dev_profile),
+            ('preprod', args.preprod_account, args.preprod_profile),
+            ('prod', args.prod_account, args.prod_profile)
+        ])]:
+            st = {
+                'set_name': bu,
+                'stages': []
+            }
+            conf_dict['cdk_app_config']['deployments'].append(st)
+            for stage, account, profile in stages:
+                st['stages'].append(
+                    {
+                        'stage_name': stage,
+                        'account': account,
+                        'bootstrap': {
+                            'aws_profile': profile
+                        }
+                    }
+                )
+
+        base_dir: str = os.path.abspath(f'{os.path.dirname(__file__)}{os.path.sep}..')
+
+        yaml_config_path: str = os.path.join(
+            base_dir,
+            'config',
+            cls.CONFIG_YAML_FILE_NAME
+        )
+
+        if os.path.exists(yaml_config_path) is False:
+            with open(yaml_config_path, 'w', encoding='utf-8') as yml_stream:
+                yaml.dump(conf_dict, yml_stream, default_flow_style=False, sort_keys=False)
+
+    @staticmethod
+    def parse_cli_args() -> Any:
+
+        parser = argparse.ArgumentParser("MLOps Cdk app configuration.")
+
+        for ctype in ['gov', 'dev', 'preprod', 'prod']:
+            ctx: str = 'Governance/Pipeline' if ctype == 'gov' else ctype
+            for cname in ['account', 'region', 'profile']:
+                cli_mame: str = f"{ctype}_{cname}"
+                parser.add_argument(
+                    f"-{cli_mame}",
+                    f"--{cli_mame}",
+                    dest=cli_mame,
+                    type=str,
+                    help=f"{ctx.capitalize()} AWS {cname.capitalize()}",
+                )
+
+        args = parser.parse_args(args=sys.argv[2:])
+
+        if args.gov_account is None or args.gov_region is None or args.gov_profile is None or \
+                args.dev_account is None or args.dev_profile is None or \
+                args.preprod_account is None or args.preprod_profile is None or \
+                args.prod_account is None or args.prod_profile is None:
+            parser.print_help()
+            sys.exit(2)
+        return args
+
 
 # used for shell script to get attribute value
 if __name__ == '__main__':
     if 'get_governance_profile' in sys.argv:
         print(ConfigHelper.get_config().pipeline.bootstrap.aws_profile)
+    elif 'create_config_file' in sys.argv:
+        ConfigHelper.create_config_file()
--- mlops-multi-account-cdk/mlops-multi-account.sh
+++ mlops-multi-account-cdk/mlops-multi-account.sh
@@ -12,6 +12,10 @@ main() {
   echo "Project Type : $cmd"
 
   case $cmd in
+    create_accounts)
+      echo "Start creating aws account using isengard and then create cdk app config"
+      "$SCRIPT_PATH"/mlops-commons/mlops_commons/scripts/isen.sh
+      ;;
     setup)
       echo "Start setting up aws accounts, python environments and it's dependencies, nodejs and it's dependencies"
       "$SCRIPT_PATH"/mlops-commons/mlops_commons/scripts/setup.sh "$SCRIPT_PATH/mlops-infra" "$SCRIPT_PATH/mlops-sm-project-template"
@@ -24,7 +28,7 @@ main() {
       echo "Start executing mlops project template"
       "$SCRIPT_PATH"/mlops-sm-project-template/scripts/mlops-template.sh "${@:2}"
       ;;
-    *) echo "Not supported command : $cmd!!!, Supported commands : [setup, infra, template]"
+    *) echo "Not supported command : $cmd!!!, Supported commands : [create_accounts, setup, infra, template]"
   esac
 }
 
